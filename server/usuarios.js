509256ec03704e2a2479213b384eeb7ea5f7684c
EdiJunior2005 <edicfjunior05@gmail.com>
2024-08-20 14:54:07 -0300

    deixando mais bonito

--- server/usuarios.js
+++ server/usuarios.js
@@ -3,17 +3,17 @@ const { processamentoRequisicao } = require("./server");
 function solicitarAdicao(nome, telefones, email){
     const requisicao = {acao: 'ADICIONAR', nome, telefones, email}
     const respostaServidor = processamentoRequisicao(requisicao)
-    console.log('Resposta do servidor', respostaServidor)
+    console.log('Adicionado', respostaServidor)
 }
 function solicitarListagem(){
     const requisicao = {acao: 'LISTAR'}
     const respostaServidor = processamentoRequisicao(requisicao)
-    console.log('Resposta do servidor: ', respostaServidor)
+    console.log('lista: ', respostaServidor)
 }
 function solicitarConsulta(id){
     const requisicao = {acao: 'CONSULTAR', id}
     const respostaServidor = processamentoRequisicao(requisicao)
-    console.log('Resposta:', respostaServidor)
+    console.log('consullta:', respostaServidor)
 }
 function solicitarRemocao(id){
     const requisicao = {acao: 'REMOVER', id}
